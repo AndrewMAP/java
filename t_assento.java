22faa502acc5f0404a9572daedf16632eb1aecb0
Andrew Map <andrewmap17@gmail.com>
2018-03-20 10:25:38 +0000

    Create t_assento.java

---
+++ t_assento.java
@@ -0,0 +1,20 @@
+public class t_Assento {
+    private int idUsuario;
+    private int numAssento;
+
+    public int getIdUsuario() {
+        return idUsuario;
+    }
+
+    public void setIdUsuario(int idUsuario) {
+        this.idUsuario = idUsuario;
+    }
+
+    public int getNumAssento() {
+        return numAssento;
+    }
+
+    public void setNumAssento(int numAssento) {
+        this.numAssento = numAssento;
+    }
+}
